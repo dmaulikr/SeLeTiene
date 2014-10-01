e85ba55e60d7d721d6d7d4728cbf31510dff72c0
Jorge Raul Ovalle Zuleta <JorgeOvalle@Jorges-MacBook-Pro.local>
2014-09-30 22:59:57 -0500

    1st view completed

---
+++ Se Le Tiene/Views/LoginViewController.h
@@ -0,0 +1,16 @@
+//
+//  LoginViewController.h
+//  Se Le Tiene
+//
+//  Created by Jorge Raul Ovalle Zuleta on 9/30/14.
+//  Copyright (c) 2014 Olinguito. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+
+@property (strong,nonatomic) IBOutlet UIButton *btnOffer;
+
+@end
---
+++ Se Le Tiene/Views/LoginViewController.m
@@ -0,0 +1,45 @@
+//
+//  LoginViewController.m
+//  Se Le Tiene
+//
+//  Created by Jorge Raul Ovalle Zuleta on 9/30/14.
+//  Copyright (c) 2014 Olinguito. All rights reserved.
+//
+
+#import "LoginViewController.h"
+
+@interface LoginViewController ()
+
+@end
+
+@implementation LoginViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+     self.view.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"bg.jpg"]];
+    self.btnOffer.titleLabel.numberOfLines = 2;
+    self.btnOffer.titleLabel.lineBreakMode = NSLineBreakByWordWrapping;
+    self.btnOffer.titleLabel.textAlignment = NSTextAlignmentCenter;
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+
+- (IBAction)getInformation:(id)sender {
+    NSLog(@"Funciona Boton");
+    
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
